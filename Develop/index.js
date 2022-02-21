7a21dfeac718ccd4646a7c27c3b77af073162965
EddieLo <eddarlostudy@yahoo.com>
2022-02-21 03:17:55 -0800

    questions added to an array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt ([
+        {   // provide project name
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your Project? (Required)',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('Please enter Project name!');
+                    return false;
+                }
+            }
+        },
+        {   // provide project description
+            type: 'input',
+            name: 'desciption',
+            message: 'Please provide a description of your Project (Required)',
+            validate: projectInfo => {
+                if (projectInfo){
+                    return true;
+                } else {
+                    console.log('Please provide project description!');
+                    return false;
+                }
+            } 
+        },
+        {   // provide install instructions
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+            validate: projectInstall => {
+                if (projectInstall){
+                    return true;
+                } else{
+                    console.log('Please enter steps required to install your project!');
+                    return false;
+                }
+            }
+        },
+        {   // provide information on how to use users app
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use this app? (Required)',
+            validate: usageInfo => {
+                if (usageInfo) {
+                    return true;
+                } else {
+                    console.log('Please enter a usage description!');
+                    return false;
+                }
+            }
+        },
+        {   // provide what the user can contribute
+            type: 'input',
+            name: 'contributors',
+            message: 'What does the user need to know about contributing to the repo'
+        },
+        {   // what test should be ran
+            type: 'input',
+            name: 'test',
+            message: 'What command should be ran to run test?',
+            default: 'npm test'
+        },
+
+
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
